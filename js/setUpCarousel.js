2ec480ec227dcf7c7c13b896b8d761b2548e1ff9
Son Giang <stakashi1311@gmail.com>
2022-08-06 01:30:09 +0930

    done

---
+++ js/setUpCarousel.js
@@ -0,0 +1,57 @@
+// Carousel courses
+$(".courses__carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
+// Carousel viewing
+$(".viewing__carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
+
+  // Carousel students rating
+
+  $(".students__carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
